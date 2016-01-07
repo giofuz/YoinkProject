339bc8c11c77d1c39db583b6f93b01f0eeb9eae1
giofuz <giovannifusciardi@hotmail.com>
2016-01-07 23:19:49 +0000

    Favourite&map icons added to menu

--- app/src/main/java/com/example/cyncyn/YoinkProject/MainActivity.java
+++ app/src/main/java/com/example/cyncyn/YoinkProject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.cyncyn.YoinkProject;
 
+
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -15,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setIcon(R.drawable.mark1);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
+//        MenuInflater mif = getMenuInflater();
+//        mif.inflate(R.menu.menu_main, menu);
+//        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -36,13 +47,20 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_map_icon) {
+
+            Intent registerIntent = new Intent(this, MapsActivity.class);
+            startActivity(registerIntent);
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
     public void btnClickHandler(View view) {
-        Intent registerIntent = new Intent(this, MapsActivity.class);
-        
-        startActivity(registerIntent);
+//        Intent registerIntent = new Intent(this, MapsActivity.class);
+//
+//        startActivity(registerIntent);
 
     }
 
